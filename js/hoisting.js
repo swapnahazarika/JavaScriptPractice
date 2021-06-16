28c959a051bd78c4577b6031b5e0d260544fb4dc
swapnahazarika <swapnahazarika20@gmail.com>
2021-06-16 09:57:36 +0530

    JS Practice work

---
+++ js/hoisting.js
@@ -0,0 +1,27 @@
+//Example of Hoisiting in JavaScript//
+var x = 1;
+console.log(x);
+a();
+b();
+
+function a() {
+    var x = 10;
+    console.log(x);
+}
+
+function b() {
+    var x = 100;
+    console.log(x);
+}
+// var x = 10;
+
+// function getName() {
+//     console.log("Hello Swapna");
+
+// }
+// getName();
+// console.log(x);
+// console.log(getName);
+// var getName = () => {
+//     console.log("Hello Swapna");
+// }
